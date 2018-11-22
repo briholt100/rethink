6acdb8353be7236f81d0a1f3d3456ca6c62a7fe6
Brian Holt <brian.c.holt@gmail.com>
2018-11-21 16:24:44 -0800

    got rstan installed, added some test code

--- rethinkStats.r
+++ rethinkStats.r
@@ -163,14 +163,86 @@ plot(sample(sample_mu,100,replace=T),sample(sample_sigma,100,replace=T))
 
 
 
-#Chapter 4 linear
+data(Howell1)
+d <- Howell1
 
-m4.3 <- map(
-  alist(
-    height ~ dnorm( mu , sigma ) ,
-    mu ~ a + b*weight,
-    a ~ dnorm( 156 , 100 ) ,
-    b ~ dnorm (0,10),
-    sigma ~ dunif( 0 , 50 )
-  ) ,
-  data=d2 )
+## R code 4.8
+str( d )
+
+## R code 4.9
+d$height
+
+## R code 4.10
+d2 <- d[ d$age >= 18 , ]
+
+## R code 4.11
+curve( dnorm( x , 178 , 20 ) , from=100 , to=250 )
+
+## R code 4.12
+curve( dunif( x , 0 , 50 ) , from=-10 , to=60 )
+
+## R code 4.13
+sample_mu <- rnorm( 1e4 , 178 , 20 )
+sample_sigma <- runif( 1e4 , 0 , 50 )
+prior_h <- rnorm( 1e4 , sample_mu , sample_sigma )
+dens( prior_h )
+
+## R code 4.14
+mu.list <- seq( from=140, to=160 , length.out=200 )
+sigma.list <- seq( from=4 , to=9 , length.out=200 )
+post <- expand.grid( mu=mu.list , sigma=sigma.list )
+post$LL <- sapply( 1:nrow(post) , function(i) sum( dnorm(
+  d2$height ,
+  mean=post$mu[i] ,
+  sd=post$sigma[i] ,
+  log=TRUE ) ) )
+post$prod <- post$LL + dnorm( post$mu , 178 , 20 , TRUE ) +
+  dunif( post$sigma , 0 , 50 , TRUE )
+post$prob <- exp( post$prod - max(post$prod) )
+
+## R code 4.15
+contour_xyz( post$mu , post$sigma , post$prob )
+
+## R code 4.16
+image_xyz( post$mu , post$sigma , post$prob )
+
+## R code 4.17
+sample.rows <- sample( 1:nrow(post) , size=1e4 , replace=TRUE ,
+                       prob=post$prob )
+sample.mu <- post$mu[ sample.rows ]
+sample.sigma <- post$sigma[ sample.rows ]
+
+## R code 4.18
+plot( sample.mu , sample.sigma , cex=0.5 , pch=16 , col=col.alpha(rangi2,0.1) )
+
+## R code 4.19
+dens( sample.mu )
+dens( sample.sigma )
+
+## R code 4.20
+HPDI( sample.mu )
+HPDI( sample.sigma )
+
+## R code 4.21
+d3 <- sample( d2$height , size=20 )
+
+## R code 4.22
+mu.list <- seq( from=150, to=170 , length.out=200 )
+sigma.list <- seq( from=4 , to=20 , length.out=200 )
+post2 <- expand.grid( mu=mu.list , sigma=sigma.list )
+post2$LL <- sapply( 1:nrow(post2) , function(i)
+  sum( dnorm( d3 , mean=post2$mu[i] , sd=post2$sigma[i] ,
+              log=TRUE ) ) )
+post2$prod <- post2$LL + dnorm( post2$mu , 178 , 20 , TRUE ) +
+  dunif( post2$sigma , 0 , 50 , TRUE )
+post2$prob <- exp( post2$prod - max(post2$prod) )
+sample2.rows <- sample( 1:nrow(post2) , size=1e4 , replace=TRUE ,
+                        prob=post2$prob )
+sample2.mu <- post2$mu[ sample2.rows ]
+sample2.sigma <- post2$sigma[ sample2.rows ]
+plot( sample2.mu , sample2.sigma , cex=0.5 ,
+      col=col.alpha(rangi2,0.1) ,
+      xlab="mu" , ylab="sigma" , pch=16 )
+
+## R code 4.23
+dens( sample2.sigma , norm.comp=TRUE )
