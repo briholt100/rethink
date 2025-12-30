aea20633012f58aa49bef5499f953ac3a116bb84
Brian Holt <brian.holt@seattlecolleges.edu>
2025-12-29 20:39:22 -0800

    added rethinking package and dens

--- MCMC_simp.r
+++ MCMC_simp.r
@@ -1,5 +1,7 @@
 #MCMC from his book
 
+library(rethinking)
+
 n_samples <- 1000
 p <- rep(NA,n_samples)
 p[1] <- 0.5
@@ -7,16 +9,18 @@ W  <- 6
 L <- 3
 for(i in 2:n_samples){
   p_new <- rnorm(1, p[i-1], 0.1)
-  print(paste0( "p_new is ",p_new))
+  # print(paste0( "p_new is ",p_new))
   if(p_new<0) p_new <- abs(p_new)
   if(p_new>1) p_new <- 2-p_new
   q0 <- dbinom(W,W+L,p[i-1])
   q1 <- dbinom(W,W+L,p_new)
-  print(paste0("q0 is ",q0))
-  print(paste0("q1 is ",q1))
+  # print(paste0("q0 is ",q0))
+  # print(paste0("q1 is ",q1))
   p[i] <- ifelse(runif(1) < q1/q0, p_new,p[i-1])
 
-    print(paste0("new p[i] is ",p[i]))
+    # print(paste0("new p[i] is ",p[i]))
   }
-p_new
-plot(p)
+p
+hist(p)
+dens(p)
+curve(dbeta(x,W+1,L+1), lty=2, add=T)
